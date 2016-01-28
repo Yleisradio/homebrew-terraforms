6e176b6a6973b4a8e288b8eea09e0ebdcba4c504
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2016-01-28 14:28:51 +0200

    Add Terraform 0.6.10

---
+++ Casks/terraform-0.6.10.rb
@@ -0,0 +1,10 @@
+cask 'terraform-0.6.10' do
+  version '0.6.10'
+  sha256 '9009582111ba938bd7e22767f533c712fb763dffa9f390b40b17f18742bfac59'
+
+  # hashicorp.com is the official download host per the vendor homepage
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
+  name 'Terraform'
+  homepage 'https://www.terraform.io/'
+  license :mpl
+end
