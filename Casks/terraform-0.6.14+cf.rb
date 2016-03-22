b568ed05e899564df22fdd2461f7f67e2d18a144
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2016-03-22 18:34:10 +0200

    Add Terraform 0.6.14+cf
    Custom build of the CloudFront support pull request (hashicorp/terraform#5221)
    rebased onto v0.6.14 release.

---
+++ Casks/terraform-0.6.14+cf.rb
@@ -0,0 +1,9 @@
+cask 'terraform-0.6.14+cf' do
+  version '0.6.14+cf'
+  sha256 '665280a03157e5a992891101aae2ea75e9a7f14076b1eb248db1717c2e7d02aa'
+
+  url "https://homebrew-terraforms.s3.amazonaws.com/terraform_0.6.14%2Bcf_darwin_amd64.zip"
+  name 'Terraform'
+  homepage 'https://www.terraform.io/'
+  license :mpl
+end
