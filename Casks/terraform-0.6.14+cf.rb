240790d67cea0bd3182d47b2a15614695ee617eb
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2016-03-31 10:52:31 +0300

    Revert to using the old version string for Terraform 0.6.14+cf
    Ugly to use the same version, but otherwise `chtf` doesn't work
    correctly. Even now the Cask needs to be manually reinstalled for
    update.

--- Casks/terraform-0.6.14+cf.rb
+++ Casks/terraform-0.6.14+cf.rb
@@ -1,5 +1,5 @@
 cask 'terraform-0.6.14+cf' do
-  version '0.6.14+cf2'
+  version '0.6.14+cf'
   sha256 'e8dada05fc7c41af6430e2ac87b93d8fd002c91ca9acbd79a02c86281ee0838f'
 
   url "https://homebrew-terraforms.s3.amazonaws.com/terraform_0.6.14%2Bcf_darwin_amd64.zip"
