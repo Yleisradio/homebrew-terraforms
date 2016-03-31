83b7a2aa49de3258d45d45e0d9f4df0399e94d2b
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2016-03-31 10:49:12 +0300

    Update Terraform 0.6.14+cf
    Includes support for ACM certificates.

--- Casks/terraform-0.6.14+cf.rb
+++ Casks/terraform-0.6.14+cf.rb
@@ -1,6 +1,6 @@
 cask 'terraform-0.6.14+cf' do
-  version '0.6.14+cf'
-  sha256 '665280a03157e5a992891101aae2ea75e9a7f14076b1eb248db1717c2e7d02aa'
+  version '0.6.14+cf2'
+  sha256 'e8dada05fc7c41af6430e2ac87b93d8fd002c91ca9acbd79a02c86281ee0838f'
 
   url "https://homebrew-terraforms.s3.amazonaws.com/terraform_0.6.14%2Bcf_darwin_amd64.zip"
   name 'Terraform'
