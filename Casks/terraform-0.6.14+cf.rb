d55bb44cdffeb978dea1c3661f2a57632ffee0b6
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2016-04-20 13:29:42 +0300

    Update Terraform 0.6.14+cf again
    Fixes the ACM certificate variable.

--- Casks/terraform-0.6.14+cf.rb
+++ Casks/terraform-0.6.14+cf.rb
@@ -1,6 +1,6 @@
 cask 'terraform-0.6.14+cf' do
   version '0.6.14+cf'
-  sha256 'e8dada05fc7c41af6430e2ac87b93d8fd002c91ca9acbd79a02c86281ee0838f'
+  sha256 '29b2f3ea691084a7f85504c39d07f84d7e8929efacaa30135470aebbbb26e53c'
 
   url "https://homebrew-terraforms.s3.amazonaws.com/terraform_0.6.14%2Bcf_darwin_amd64.zip"
   name 'Terraform'
