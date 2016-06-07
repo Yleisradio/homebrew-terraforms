04f3824f5a6b65f466227845457bc9d8ad33f069
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2016-06-07 09:42:42 +0300

    Add Terraform 0.7.0-rc1

---
+++ Casks/terraform-0.7.0-rc1.rb
@@ -0,0 +1,10 @@
+cask 'terraform-0.7.0-rc1' do
+  version '0.7.0-rc1'
+  sha256 '04cb5d3fdb500ac83a2002006b4331a9e5db92c22b8b4971731c66a9fb8906ee'
+
+  # hashicorp.com is the official download host per the vendor homepage
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
+  name 'Terraform'
+  homepage 'https://www.terraform.io/'
+  license :mpl
+end
