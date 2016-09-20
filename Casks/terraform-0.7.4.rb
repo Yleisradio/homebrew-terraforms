396d975e2da448155ab74263f5ad411b66e401cd
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2016-09-20 11:36:37 +0300

    Add Terraform 0.7.4

---
+++ Casks/terraform-0.7.4.rb
@@ -0,0 +1,9 @@
+cask 'terraform-0.7.4' do
+  version '0.7.4'
+  sha256 '21c8ecc161628ecab88f45eba6b5ca1fbf3eb897e8bc951b0fbac4c0ad77fb04'
+
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
+  name 'Terraform'
+  homepage 'https://www.terraform.io/'
+  license :mpl
+end
