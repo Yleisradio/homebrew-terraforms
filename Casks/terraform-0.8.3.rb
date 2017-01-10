c28ac79a56e050b7a0cc0addafd41e886e9f5fee
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2017-01-10 11:18:49 +0200

    Add Terraform 0.8.3

---
+++ Casks/terraform-0.8.3.rb
@@ -0,0 +1,9 @@
+cask 'terraform-0.8.3' do
+  version '0.8.3'
+  sha256 '84ecdd2adf61629a6bd4c1316df8f76290afad689630225d415666b422214a83'
+
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
+  name 'Terraform'
+  homepage 'https://www.terraform.io/'
+  license :mpl
+end
