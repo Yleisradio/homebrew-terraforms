f43f6ebfbe3fe76839128a0413500f5c02a30ec5
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2017-03-03 10:18:38 +0200

    Add Terraform 0.9.0-beta2

---
+++ Casks/terraform-0.9.0-beta2.rb
@@ -0,0 +1,9 @@
+cask 'terraform-0.9.0-beta2' do
+  version '0.9.0-beta2'
+  sha256 'b102ba7481e1310d96e70f499c58de48a753a45496d093d63897a3bc432636c2'
+
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
+  name 'Terraform'
+  homepage 'https://www.terraform.io/'
+  license :mpl
+end
