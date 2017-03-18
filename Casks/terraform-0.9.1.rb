e9a6c7e17a29f5228882ac31adb60ce6cc20d7a4
Laurent Commarieu <laurent.commarieu@gmail.com>
2017-03-18 08:20:26 +0100

    Create terraform-0.9.1.rb

---
+++ Casks/terraform-0.9.1.rb
@@ -0,0 +1,9 @@
+cask 'terraform-0.9.1' do
+  version '0.9.1'
+  sha256 '4140c52917da91a276db34f01e5efc27d07b6e1deeede4137625fccf7bfabb83'
+
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
+  name 'Terraform'
+  homepage 'https://www.terraform.io/'
+  license :mpl
+end
