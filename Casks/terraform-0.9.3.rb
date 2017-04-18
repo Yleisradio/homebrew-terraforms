622e3d938a0b9c2ff3fb16381190260aec53e3fa
Vitalii Chernomor <vchernomor@callfire.com>
2017-04-19 01:39:15 +0300

    Add Terraform 0.9.3

---
+++ Casks/terraform-0.9.3.rb
@@ -0,0 +1,9 @@
+cask 'terraform-0.9.3' do
+  version '0.9.3'
+  sha256 '180afdeb14f4049f3374fe02b9143ad428ebd31dd89c6595775d7ba439d7fbf0'
+
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
+  name 'Terraform'
+  homepage 'https://www.terraform.io/'
+  license :mpl
+end
