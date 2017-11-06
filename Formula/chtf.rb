144065ab7c55a53bd08a396e3d4609f997f52504
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2017-11-06 11:50:48 +0200

    Update the chtf Homebrew Formula for v1.3.0

--- Formula/chtf.rb
+++ Formula/chtf.rb
@@ -1,8 +1,8 @@
 class Chtf < Formula
   desc 'Terraform version switcher'
   homepage 'https://github.com/Yleisradio/homebrew-terraforms#readme'
-  url 'https://github.com/Yleisradio/homebrew-terraforms/archive/v1.2.1.tar.gz'
-  sha256 'f87445f036e8fc665a64559fb0d4a12697e973714ffebee406f72f419912f0cd'
+  url 'https://github.com/Yleisradio/homebrew-terraforms/archive/v1.3.0.tar.gz'
+  sha256 '31bf27a5ccab98942159a94a13c530e951deafb00c9b431535462003d8d7bdfe'
 
   head 'https://github.com/Yleisradio/homebrew-terraforms.git'
 
