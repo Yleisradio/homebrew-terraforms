801272a3b1a8d845fc7475e6f4ae8611e5176cd3
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2018-04-23 15:19:43 +0300

    [chtf] Improve zsh completion
    Only allow one option/argument.

--- etc/chtf-completion.zsh
+++ etc/chtf-completion.zsh
@@ -2,8 +2,8 @@
 
 function _chtf() {
     _arguments \
-        -h --help -V --version \
-        '1:Terraform version:($(chtf | tr -d " *") system)'
+        '(- 1)'{-h,--help,-V,--version} \
+        '(-)1:Terraform version:($(chtf | tr -d " *") system)'
 }
 
 _chtf
