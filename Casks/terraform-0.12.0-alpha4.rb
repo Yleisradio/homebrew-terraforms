c88b907fe46f3e8fd7975a9085f4216c05c86774
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2018-12-18 10:22:16 +0200

    Fix the strange download url for Terraform 0.12.0-alpha4

--- Casks/terraform-0.12.0-alpha4.rb
+++ Casks/terraform-0.12.0-alpha4.rb
@@ -2,7 +2,8 @@ cask 'terraform-0.12.0-alpha4' do
   version '0.12.0-alpha4'
   sha256 'ff742e857fe37e76747f099cf99f043c36d408c62cefba6a700e3c9c118e0690'
 
-  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
+  # XXX: Strange URL for 0.12.0-alpha4
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_terraform_#{version}_darwin_amd64.zip"
   name 'Terraform'
   homepage 'https://www.terraform.io/'
 end
