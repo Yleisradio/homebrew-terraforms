5ee08a851e09453414ac6f67ebcfae5194153120
ttalbert7 <ttalbert7@gmail.com>
2019-07-19 02:28:18 -0400

    Add terraform 0.12.4 (#29)

---
+++ Casks/terraform-0.12.4.rb
@@ -0,0 +1,7 @@
+cask 'terraform-0.12.4' do
+  version '0.12.4'
+  sha256 'e19691d775849888a0695a07e52a884dc617ca2100759eca5bbe4d0f428a7bc3'
+  url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
+  name 'Terraform'
+  homepage 'https://www.terraform.io/'
+end
