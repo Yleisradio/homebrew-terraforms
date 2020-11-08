8ff61dda6919f55de6fc4698653da9ee51649ba6
Teemu Matilainen <teemu.matilainen@iki.fi>
2020-11-08 18:36:52 +0200

    Format all existing Casks based on the template

--- Casks/terraform-0.6.15.rb
+++ Casks/terraform-0.6.15.rb
@@ -2,7 +2,6 @@ cask 'terraform-0.6.15' do
   version '0.6.15'
   sha256 '9cb305ac00b85e2575da3c71504f3fdd3f7ef61f35457af999c7b88802143311'
 
-  # hashicorp.com is the official download host per the vendor homepage
   url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
   name 'Terraform'
   homepage 'https://www.terraform.io/'
--- Casks/terraform-0.6.16.rb
+++ Casks/terraform-0.6.16.rb
@@ -2,7 +2,6 @@ cask 'terraform-0.6.16' do
   version '0.6.16'
   sha256 '23feb79263126877e6128a03c600cd626f6691a118a474694c5ad45cc5da9366'
 
-  # hashicorp.com is the official download host per the vendor homepage
   url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
   name 'Terraform'
   homepage 'https://www.terraform.io/'
